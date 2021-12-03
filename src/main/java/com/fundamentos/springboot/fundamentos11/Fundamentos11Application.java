11fc11b4e7926d007c98e0eb5b08d593d758b95b
Ronal Mendoza <ronalmendozat@outlook.com>
2021-12-02 21:24:29 -0500

    primer commit

---
+++ src/main/java/com/fundamentos/springboot/fundamentos11/Fundamentos11Application.java
@@ -0,0 +1,14 @@
+package com.fundamentos.springboot.fundamentos11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fundamentos11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fundamentos11Application.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/fundamentos/springboot/fundamentos11/Fundamentos11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.fundamentos11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fundamentos11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
